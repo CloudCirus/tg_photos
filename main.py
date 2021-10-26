e00dd4562bbef3426fdbe8f59281af6b52e3642c
Cloud <smirnovceo@yandex.ru>
2021-10-26 18:44:31 +0500

    Get last launch with imgs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import sys
+from pprint import pprint
 
 import requests
 
@@ -17,6 +17,33 @@ def download_picture(url: str, filename: str) -> None:
         f.write(resp.content)
 
 
+def get_all_launches_id(url: str):
+    resp_all_launches = requests.get(url).json()
+    return [lounch['id'] for lounch in resp_all_launches]
+
+
+def get_last_launch_with_img(launches_id: list, url: str) -> dict:
+    url_launches = f'https://api.spacexdata.com/v5/launches/'
+    last_launch_with_imgs = {}
+    counter = 1
+    print(counter)
+    for id in reversed(launches_id):
+        url = f'{url_launches}{id}'
+        resp = requests.get(url).json()
+        launch_imgs = resp['links']['flickr'].get('original')
+        if launch_imgs:
+            last_launch_with_imgs = {
+                'flight_number': resp.get('flight_number'),
+                'id': id,
+                'links': launch_imgs
+            }
+            break
+        counter += 1
+        print(counter)
+    return last_launch_with_imgs
+
+
 if __name__ == '__main__':
-    hubble_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    download_picture(hubble_url, 'hubble.jpeg')
+    url_launches = 'https://api.spacexdata.com/v5/launches/'
+    launches_id = get_all_launches_id(url_launches)
+    pprint(get_last_launch_with_img(launches_id, url_launches))
