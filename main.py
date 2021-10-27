100751eb91104e22539061d2a71b9077e119f548
Cloud <smirnovceo@yandex.ru>
2021-10-27 18:27:14 +0500

    Fetch nasa imgs and unify downloading logic

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import os
+from datetime import datetime, timedelta
 from pprint import pprint
 from urllib.parse import urlsplit
 
 import requests
+from dotenv import load_dotenv
 
 
-def download_pictures(launch: dict) -> None:
-    flight_number = launch.get('flight_number')
-    img_path = f'images/{flight_number}'
+def download_pictures(links: list, name: str) -> None:
+    img_path = f'images/{name}'
     if not os.path.exists(img_path):
         try:
             os.makedirs(img_path)
         except FileExistsError as ex:
             print(ex)
 
-    img_links = launch.get('links')
-    for index, link in enumerate(img_links, start=1):
+    for index, link in enumerate(links, start=1):
         resp = requests.get(link)
         resp.raise_for_status()
-        with open(f'{img_path}/{flight_number}_{index}_spacex.jpg', 'wb') as f:
+        ext = _get_file_extension(link)
+        with open(f'{img_path}/{index}_{name}{ext}', 'wb') as f:
             f.write(resp.content)
 
 
-def get_all_launches_id(url: str) -> list:
+def get_all_launches_id() -> list:
+    url = f'https://api.spacexdata.com/v5/launches/'
     resp_all_launches = requests.get(url).json()
     return [lounch['id'] for lounch in resp_all_launches]
 
 
-def get_last_launch_with_img(launches_id: list, url: str) -> dict:
+def get_last_launch_img_links(launches_id: list) -> list:
     url_launches = f'https://api.spacexdata.com/v5/launches/'
     last_launch_with_imgs = {}
     counter = 1
@@ -45,21 +47,46 @@ def get_last_launch_with_img(launches_id: list, url: str) -> dict:
             break
         counter += 1
         print(counter)
-    return last_launch_with_imgs
+    return last_launch_with_imgs.get('links')
 
 
 def fetch_spacex_last_launch() -> None:
-    url_launches = 'https://api.spacexdata.com/v5/launches/'
-    launches_id = get_all_launches_id(url_launches)
-    last_launch_with_img = get_last_launch_with_img(launches_id, url_launches)
-    download_pictures(last_launch_with_img)
+    launches_id = get_all_launches_id()
+    last_launch_with_img = get_last_launch_img_links(launches_id)
+    download_pictures(last_launch_with_img, 'spacex')
 
 
-def get_file_extension(url):
+def _get_file_extension(url: str) -> str:
     path = urlsplit(url).path
     return os.path.splitext(path)[1]
-    
+
+
+def get_apod_img_links(days: int) -> None:
+    load_dotenv()
+    url = 'https://api.nasa.gov/planetary/apod'
+    options = {
+        'api_key': os.environ.get('NASA_API_KEY'),
+        'start_date': (datetime.now() - timedelta(days)).strftime('%Y-%m-%d'),
+        'end_date': datetime.now().strftime('%Y-%m-%d'),
+    }
+    resp = requests.get(url, params=options).json()
+    links = []
+    for day in resp:
+        if day.get('media_type') == 'image':
+            url = day.get('url')
+            if url:
+                links.append(url)
+    return links
+
+
+def fetch_nasa_apods() -> None:
+    links = get_apod_img_links(10)
+    download_pictures(links, 'nasa')
+
 
 if __name__ == '__main__':
     # fetch_spacex_last_launch()
-    print(get_file_extension('https://example.com/txt/hello%20world.txt?v=9#python'))
+    # print(get_file_extension('https://example.com/txt/hello%20world.txt?v=9#python'))
+    # res = get_apod(30)
+    # pprint(res)
+    fetch_nasa_apods()
