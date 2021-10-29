cd4857a73c8643fe5fb1eb6b854a64067a7d105d
Cloud <smirnovceo@yandex.ru>
2021-10-29 18:46:20 +0500

    Publish photo to telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -1,18 +1,31 @@
 import os
 
+import random
 from dotenv import load_dotenv
 from telegram import Bot
 
 
 def send_message() -> None:
-    load_dotenv()
     TOKEN = os.environ.get('TG_TOKEN')
 
     bot = Bot(token=TOKEN)
 
     while input('Send message? y/n >>> ') in ['y', 'Y']:
-        bot.send_message(chat_id='@WowSpaceClose', text=input('input text >>> '))
+        bot.send_message(chat_id='@WowSpaceClose',
+                         text=input('input text >>> '))
+
+
+def send_photo() -> None:
+    TOKEN = os.environ.get('TG_TOKEN')
+
+    bot = Bot(token=TOKEN)
+
+    while input('Send photo? y/n >>> ') in ['y', 'Y']:
+        index = random.randint(1, 10)
+        bot.send_photo(chat_id='@WowSpaceClose',
+                       photo=open(f'images/nasa/{index}_nasa.jpg', 'rb'))
 
 
 if __name__ == '__main__':
-    send_message()
+    load_dotenv()
+    send_photo()
