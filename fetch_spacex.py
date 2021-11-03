9f37505459061875a40a0b0f57adb9ea95ca343d
Cloud <smirnovceo@yandex.ru>
2021-11-04 02:26:40 +0500

    Lesscode with get spx last launch links

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,35 +3,24 @@ import requests
 from storage import download_pictures
 
 
-def get_all_spx_launches_id() -> list:
+def get_spx_last_launch_img_links() -> list:
     url = f'https://api.spacexdata.com/v5/launches/'
-    resp = requests.get(url)
-    resp.raise_for_status()
-    return [launch['id'] for launch in resp.json()]
+    all_launches_resp = requests.get(url)
+    all_launches_resp.raise_for_status()
 
-
-def get_spx_last_launch_img_links(launches_id: list) -> list:
-    url_launches = f'https://api.spacexdata.com/v5/launches/'
-    last_launch_with_imgs = {}
-    counter = 1
-    for id in reversed(launches_id):
-        url = f'{url_launches}{id}'
-        resp = requests.get(url).json()
-        launch_imgs = resp['links']['flickr'].get('original')
+    for launch in reversed(all_launches_resp.json()):
+        launch_imgs = launch['links']['flickr'].get('original')
         if launch_imgs:
             last_launch_with_imgs = {
-                'flight_number': resp.get('flight_number'),
-                'id': id,
-                'links': launch_imgs
+                'flight_number': launch.get('flight_number'),
+                'links': launch_imgs,
             }
             break
-        counter += 1
     return last_launch_with_imgs.get('links')
 
 
 def fetch_spacex_last_launch() -> None:
-    launches_id = get_all_spx_launches_id()
-    last_launch = get_spx_last_launch_img_links(launches_id)
+    last_launch = get_spx_last_launch_img_links()
     download_pictures(last_launch, 'spacex')
 
 
