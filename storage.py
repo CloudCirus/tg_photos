f33cb70c5584b24e10915ddbdc2974b9985c777e
Cloud <smirnovceo@yandex.ru>
2021-11-04 00:28:26 +0500

    Refactor dir creation with lesscode

--- storage.py
+++ storage.py
@@ -7,10 +7,7 @@ import requests
 def download_pictures(links: list, whose_pic_name: str) -> None:
     path = f'images/{whose_pic_name}'
     if not os.path.exists(path):
-        try:
-            os.makedirs(path)
-        except FileExistsError as ex:
-            print(ex)
+        os.makedirs(path)
 
     for index, link in enumerate(links, start=1):
         resp = requests.get(link)
